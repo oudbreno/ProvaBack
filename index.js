c97e1041fe9368bf3a81da2b03c02a363bdc9ab1
BERNARDO ARAUJO BARBOSA <bernardo_a_barbosa@estudante.sesisenai.org.br>
2025-05-07 12:03:08 -0300

    Alteracoes Express

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const fs = require("fs")
 const {v4: gerarId} = require("uuid")
-function novoUsuario(nome){
+const express = require('express')
+const app = express();
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+  });
+
+app.use(express.json());
+
+app.post('/cadastro', (req,res) =>{
+    const {nome} = req.body;
+    if(!nome){
+        return res.status(400).json;
+    }
+
     const id = gerarId();
     const data = new Date().toLocaleDateString('pt-BR');
     const mensagem = `${id} - ${data} - ${nome}\n`;
 
-fs.appendFileSync('logs.txt', mensagem);
-console.log(id)
+    fs.appendFileSync('logs.txt', mensagem);
+    res.json({ id, mensagem: "Log criado com sucesso!" });
 
-}
+})
 
-novoUsuario("breno")
