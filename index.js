6dddaaafff38061f64fc9eb7e529548ea1c576e8
BERNARDO ARAUJO BARBOSA <bernardo_a_barbosa@estudante.sesisenai.org.br>
2025-05-07 11:30:40 -0300

    Funcao De Novo Usuario

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const fs = require("fs")
 const {v4: gerarId} = require("uuid")
 function novoUsuario(nome){
     const id = gerarId();
-    const data = Date.toISOString();
-    const mensagem = `${id} - ${data} - ${nome}`;
+    const data = new Date().toLocaleDateString('pt-BR');
+    const mensagem = `${id} - ${data} - ${nome}\n`;
+
+fs.appendFileSync('logs.txt', mensagem);
+console.log(id)
+
 }
 
+novoUsuario("breno")
